2ad2955e6527ea2846ac6e270f95f5d2f67ff11f
khldoon <oi18aan@cs.umu.se>
2023-03-16 20:00:53 +0100

    Fixed some styling

--- src/components/AccordionMenu/AccordionMenu.js
+++ src/components/AccordionMenu/AccordionMenu.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-
 const AccordionMenu = () => {
+	const [expanded, setExpanded] = useState({});
+
+	const toggleExpansion = (id) => {
+		setExpanded({
+			...expanded,
+			[id]: !expanded[id],
+		});
+	};
 	const menuItems = [
 		{
 			id: 1,
-			title: "Appetizers",
-			items: ["Mozzarella Sticks", "Fried Calamari", "Chicken Wings"],
+			title: "Pizza",
+			items: ["Kebab pizza", "Kyckling pizza", "Salami och oliver pizza"],
 		},
 		{
 			id: 2,
-			title: "Entrees",
+			title: "Kebab",
 			items: [
 				"Spaghetti Carbonara",
 				"Chicken Parmesan",
@@ -19,18 +26,26 @@ const AccordionMenu = () => {
 		},
 		{
 			id: 3,
-			title: "Desserts",
-			items: ["Tiramisu", "Cannoli", "Chocolate Cake"],
+			title: "Kyckling",
+			items: ["Kycklingrulle", "Cannoli", "Chocolate Cake"],
 		},
 	];
-
 	return (
-		<div className="lift-container">
+		<div className="left-container">
 			<div className="accordion-menu">
 				{menuItems.map((item) => (
 					<div className="accordion-item" key={item.id}>
-						<div className="accordion-header">{item.title}</div>
-						<ul className="accordion-list">
+						<div
+							className="accordion-header"
+							onClick={() => toggleExpansion(item.id)}
+						>
+							<div className="title">{item.title}</div>
+						</div>
+						<ul
+							className={`accordion-list ${
+								expanded[item.id] ? "expanded" : ""
+							}`}
+						>
 							{item.items.map((menuItem, index) => (
 								<li key={index}>{menuItem}</li>
 							))}
@@ -41,5 +56,4 @@ const AccordionMenu = () => {
 		</div>
 	);
 };
-
 export default AccordionMenu;
